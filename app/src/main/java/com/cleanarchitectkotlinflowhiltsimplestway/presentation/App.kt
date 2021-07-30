d1e69a6fdbb2c8b1b54d87d5230827b3d82e4d36
MilenkoMarkovic <milenko.markovic@akvelon.com>
2021-07-30 10:33:49 +0200

    App Center analytics added

--- app/src/main/java/com/cleanarchitectkotlinflowhiltsimplestway/presentation/App.kt
+++ app/src/main/java/com/cleanarchitectkotlinflowhiltsimplestway/presentation/App.kt
@@ -2,12 +2,17 @@ package com.cleanarchitectkotlinflowhiltsimplestway.presentation
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 @HiltAndroidApp
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+	AppCenter.start(getApplication(), "955b4774-e16b-4fe1-a9ae-ccb5298923dd",
+                  Analytics.class, Crashes.class);
 
     }
 }
